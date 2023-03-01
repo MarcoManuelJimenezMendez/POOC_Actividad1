01b99bfff4d06215bd1568d5f8cc52f36d45f07a
MarcoManuelJimenezMendez <115948111+MarcoManuelJimenezMendez@users.noreply.github.com>
2023-03-01 10:24:06 -0600

    Add files via upload

---
+++ Casa1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace POOC_Actividad1;
+
+public class Casa1 
+{         
+        public string nombreDueno, apellidoDueno, calle, colonia, tamano, ubicacion;
+		public int codigoPostal, puertas, ventanas, numeroCuartos,;
+	
+		
+		
+	
+	
+		
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace POOC_Actividad1;
+
+public static class Program
+{
+	public static void Main()
+	{
+		casa1 objetoCasa = new casa1();
+		
+		
+	}
+}
